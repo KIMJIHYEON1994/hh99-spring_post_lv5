5340691f5130d7fe7c36a6b90da5088d5b125862
LEOJI <kjh11-07@hanmail.net>
2023-04-25 16:06:08 +0900

    username, password 형식 dto로 이동, password 암호화 테스트 완료

--- src/main/java/com/sparta/spring_post/entity/Users.java
+++ src/main/java/com/sparta/spring_post/entity/Users.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,14 +14,10 @@ import javax.validation.constraints.Size;
 public class Users {
     @Id
     @Column(name = "user_name", nullable = false, unique = true)
-    @Size(min = 4, max = 10, message = "아이디의 길이가 4자 이상 10자 이하로 구성되어야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]*$", message = "형식에 맞지 않는 아이디 입니다.")
     private String username;
 
     @Column(nullable = false)
     @JsonIgnore
-    @Size(min = 8, max = 15, message = "비밀번호의 길이가 8자 이상 15자 이하로 구성되어야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&])[a-zA-Z\\d~!@#$%^&]*$", message = "형식에 맞지 않는 비밀번호 입니다.")
     private String password;
 
     @Column
