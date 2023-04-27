3c6b56967117821adbb1016d80e1fa642580bea3
LEOJI <kjh11-07@hanmail.net>
2023-04-28 00:05:52 +0900

    Fix : Comment 컨트롤러 시큐리티 적용

--- src/main/java/com/sparta/spring_post/controller/CommentController.java
+++ src/main/java/com/sparta/spring_post/controller/CommentController.java
@@ -3,11 +3,11 @@ package com.sparta.spring_post.controller;
 import com.sparta.spring_post.dto.CommentRequestDto;
 import com.sparta.spring_post.dto.UserResponseDto;
 import com.sparta.spring_post.entity.Comment;
+import com.sparta.spring_post.security.UserDetailsImpl;
 import com.sparta.spring_post.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
@@ -18,20 +18,20 @@ public class CommentController {
 
     // 댓글 작성
     @PostMapping("/comment")
-    public UserResponseDto<Comment> addComment(@RequestBody CommentRequestDto commentRequestDto, HttpServletRequest httpServletRequest) {
-        return commentService.addComment(commentRequestDto, httpServletRequest);
+    public UserResponseDto<Comment> addComment(@RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.addComment(commentRequestDto, userDetails.getUser());
     }
 
     // 댓글 수정
     @PutMapping("/comment/{id}")
-    public UserResponseDto<Comment> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest httpServletRequest) {
-        return commentService.updateComment(id, commentRequestDto, httpServletRequest);
+    public UserResponseDto<Comment> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.updateComment(id, commentRequestDto, userDetails.getUser());
     }
 
     // 댓글 삭제
     @DeleteMapping("/comment/{id}")
-    public UserResponseDto<Comment> deleteComment(@PathVariable Long id, HttpServletRequest httpServletRequest) {
-        return commentService.deleteComment(id, httpServletRequest);
+    public UserResponseDto<Comment> deleteComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.deleteComment(id, userDetails.getUser());
     }
 
     // 댓글 좋아요
@@ -39,6 +39,5 @@ public class CommentController {
     public UserResponseDto<Comment> likeComment(@PathVariable Long id) {
         return commentService.likeComment(id);
     }
-
 
 }
